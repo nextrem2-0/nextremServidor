e9c1e23df9d3f8e9d664dce3076a1d01de3a3bb4
Alejandro Esteve Maza <54013692d@AD.FPMISLATA.COM>
2019-12-10 13:34:47 +0100

    feat(#0):Estructura

---
+++ app/CategoriasModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoriasModel extends Model
+{
+    protected $key = "id";
+    protected $table  = "categorias";
+}
---
+++ database/migrations/2019_12_10_094249_create_categorias_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriasTable extends Migration
+{
+    public $timestamps = false;
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre')->nullable();
+        });
+        Schema::create('deportes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre')->nullable();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias');
+        Schema::dropIfExists('deportes');
+    }
+}
