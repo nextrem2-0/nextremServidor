c7e6c0810608230bb95eb67c9bf38898c6d71a4b
Pau Hermosilla Del Rio <pauhermosillad@gmail.com>
2019-12-17 16:20:38 +0100

    fix(#11): variables globales con valores

--- config/variables.php
+++ config/variables.php
@@ -1,18 +1,20 @@
 <?php return [
     'txt0' => [
-        'imagen' => "img1",
-        'titulo' => "titulo1",
-        'texto' => "texto1",
+        'imagen' => "fa fa-crosshairs fa-7x",
+        'titulo' => "Centralizamos eventos deportivos",
+        'texto' => "Tenemos convenio con diversas empresas a nivel 
+        estatal que publican sus eventos en nuestra pagina para que los tengas todos al alcance de tu mano",
     ],
     'txt1' => [
-        'imagen' => "img2",
-        'titulo' => "titulo2",
-        'texto' => "texto2",
+        'imagen' => "fa fa-users fa-7x",
+        'titulo' => "Comunidad de deportes extremos",
+        'texto' => "Únete a otros amantes de la adrenalina y comparte momentos únicos",
     ],
     'txt2' => [
-        'imagen' => "img3",
-        'titulo' => "titulo3",
-        'texto' => "texto3",
+        'imagen' => "fa fa-chart-line fa-7x",
+        'titulo' => "Descubre tu siguiente reto",
+        'texto' => "Cada dia te ofrecemos la posibilidad de ampliar tus horizontes y practicar otros deportes 
+        gracias a las recomendaciones personalizadas",
     ],
 
 ];
