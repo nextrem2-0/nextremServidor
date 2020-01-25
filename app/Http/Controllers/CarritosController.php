16f6c0da48a55743d01c8a2ee95df434d9e3491d
unknown <alex.esteve98@gmail.com>
2020-01-25 16:33:07 +0100

    feat(#30): carrito store

--- app/Http/Controllers/CarritosController.php
+++ app/Http/Controllers/CarritosController.php
@@ -5,26 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\models\CarritosModel;
 use App\models\Carrito_EventoModel;
+use App\models\EventosModel;
 
 class CarritosController extends Controller
 {
     public function store(Request $request)
     {
-        return $request;
-        $carrito = collect();
-
-        foreach ($request->usuario_id as $usuario_id) {
-
-            $carrito->push(
-                CarritosModel::make([
-                    'usuario_id'   => $usuario_id,
-                    'precio' => $request->precio
-                ])
-            );
+        $carrito =  CarritosModel::create([
+            'usuario_id' => $request->get('idUsusario'),
+            'precio' => $request->get('precio'),
+        ]);
+        foreach ($request->eventos as $evento) {
+            Carrito_EventoModel::create([
+                'carrito_id' => $carrito->id,
+                'evento_id' => $evento['id_evento'],
+                'plazas' => $evento['plazas'],
+            ]);
+            EventosModel::where('id', $evento['id_evento'])->decrement('capacidad', $evento['plazas']);
         }
-
-        DB::table('carritos')->insert($carrito);
-
-        //return redirect()->route('project-miles.index');
     }
 }
